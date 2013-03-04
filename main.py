5e6fb73705f3d4b71a671e682a8fb14db0ee1526
Hadayat Seddiqi <hadsed@gmail.com>
2013-03-03 20:03:00 -0500

    Everything is still being weird and not moving.

--- main.py
+++ main.py
@@ -11,34 +11,37 @@ from scipy import integrate
 import pylab as pl
 import subprocess
 
-nx = 10000
-dx = 0.0001
-dt = 0.000001
+nx = 8000
+dx = 0.0025
+dt = 0.00002
 niter = 20
 nonlin = 0.0
 gridx = sp.zeros(nx)
+igridnx = sp.array(range(nx))
 psi = sp.zeros(nx)
 psi2 = sp.zeros(nx)
 pot = sp.zeros(nx)
-depth = 0.003
+depth = 0.1
 
 # Set up grid, potential, and initial state
-for i in range(0, nx):
-    gridx[i] = dx*(i - nx/2)
-    pot[i] = depth*(gridx[i]**2)
-    psi[i] = sp.exp(-0.5*50*gridx[i]**2)
-    psi2[i] = psi[i]**2
+gridx = dx*(igridnx - nx/2)
+pot = depth*(gridx*gridx)
+psi = sp.pi**(-1/4)*sp.exp(-0.5*gridx*gridx)
 
 # Normalize Psi
-psi /= sp.integrate.simps(psi2)
+psi /= sp.integrate.simps(psi*psi, dx=dx)
+
+# Plot parameters
+xlimit = [gridx[0], gridx[-1]]
+ylimit = [0, 2*psi[nx/2]]
 
 # Compute imaginary time
 alpha = sp.zeros(nx)
 beta = sp.zeros(nx)
 gamma = sp.zeros(nx)
-dx2 = dx**2
-tdD = 1 + dt/dx2
-tdS = -0.5*dt/dx2
+dx2 = dx*dx
+tdD = 1 - dt/dx2
+tdS = 0.5*dt/dx2
 bconst = 0
 alpha[nx - 1] = 0.0
 gamma[nx - 1] = -1.0/tdD
@@ -59,15 +62,14 @@ for t in range(0, niter) :
         bconst = psi[i] - (psi[i+1] - 2.0*psi[i] + psi[i-1])*tdS
         beta[i - 1] = gamma[i]*(tdS*beta[i] - bconst)
 
-    # Calculate Psi2
-    psi2 = psi**2
+    # Normalize Psi
+    psi /= sp.integrate.simps(psi*psi, dx=dx)
 
     # Output figures
     pl.plot(gridx, psi)
     pl.plot(gridx, pot)
-    pl.xlim([gridx[0], gridx[-1]])
-    pl.ylim([0, psi[nx/2] + psi[nx/4]])
+    pl.xlim(xlimit)
+    pl.ylim(ylimit)
     pl.savefig('output/fig' + str(t))
     pl.clf()
-    
-#subprocess.Popen("convert 
+
--- mainla.py
+++ mainla.py
@@ -13,42 +13,40 @@ import scipy.sparse.linalg
 import pylab as pl
 import subprocess
 
-nx = 100000
-dx = 0.0001
-dt = 0.000001
-niter = 30
+nx = 8000
+dx = 0.0025
+dt = 0.00002
+niter = 20
 nonlin = 0.0
 gridx = sp.zeros(nx)
 igridx = sp.array(range(nx))
 psi = sp.zeros(nx)
-psi2 = sp.zeros(nx)
-
 pot = sp.zeros(nx)
-depth = 0.00003
-
-Adiag = sp.empty(nx)
-Asup = sp.empty(nx)
-Asub = sp.empty(nx)
+depth = 0.1
 
 # Set up grid, potential, and initial state
 gridx = dx*(igridx - nx/2)
-pot = depth*gridx**2
-psi = sp.exp(-0.5*gridx**2)
-psi2 = psi**2
+pot = depth*gridx*gridx
+psi = sp.pi**(-1/4)*sp.exp(-0.5*gridx*gridx)
 
 # Normalize Psi
-psi /= sp.integrate.simps(psi2)
+psi /= sp.integrate.simps(psi*psi, dx=dx)
 
 # Plot parameters
 xlimit = [gridx[0], gridx[-1]]
 ylimit = [0, 2*psi[nx/2]]
 
 # Set up diagonal coefficients
-Adiag.fill(1 + dt/dx**2)
-Asup.fill(-dt/(2*dx**2))
-Asub.fill(-dt/(2*dx**2))
+Adiag = sp.empty(nx)
+Asup = sp.empty(nx)
+Asub = sp.empty(nx)
+Adiag.fill(1 - dt/dx**2)
+Asup.fill(dt/(2*dx**2))
+Asub.fill(dt/(2*dx**2))
 
-Adiag[0] = Adiag[-1] = 0
+# Impose boundary conditions
+Adiag[0] = 0
+Adiag[-1] = 0
 Asup[1] = 0
 Asub[-2] = 0
 
@@ -58,20 +56,17 @@ A = sp.sparse.spdiags([Adiag, Asup, Asub], [0, 1, -1], nx, nx)
 # Loop through time
 for t in range(0, niter) :
     # Calculate effect of potential and nonlinearity
-    psi *= sp.exp(dt*(pot + nonlin))
-
+    psi *= sp.exp(-dt*(pot + nonlin*psi*psi))
+    
     # Calculate spacial derivatives
     psi = sp.sparse.linalg.spsolve(A, psi)
 
     # Normalize Psi
-    psi /= sp.integrate.simps(psi**2)
-
-    # Plot parameters
-    xlimit = [gridx[0], gridx[-1]]
-    ylimit = [0, 2*psi[nx/2]]
+    psi /= sp.integrate.simps(psi*psi, dx=dx)
 
     # Output figures
     pl.plot(gridx, psi)
+    pl.plot(gridx, psi*psi)
     pl.plot(gridx, pot)
     pl.xlim(xlimit)
     pl.ylim(ylimit)
